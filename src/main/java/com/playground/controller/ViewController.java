7a75c72702fce91416c3c76557ae2d30e9480226
Amit Mittal <amit.mittal1993@gmail.com>
2018-08-28 16:21:08 +0530

    Using the model file to plugin values

--- src/main/java/com/playground/controller/ViewController.java
+++ src/main/java/com/playground/controller/ViewController.java
@@ -3,11 +3,14 @@ package com.playground.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class ViewController {
 
     @RequestMapping(value = "/other")
-    public String other() {
+    public String other(Map model) {
+        model.put("key1", "value1");
         return "otherFile";
     }
 }
